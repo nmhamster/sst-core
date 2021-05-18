4794758c3f6fe531318d28c2b454413561ff09d1
Si Hammond <sdhammo@sandia.gov>
2021-05-18 09:23:14 -0600

    Fix up formatting

--- src/sst/core/rng/uniform.h
+++ src/sst/core/rng/uniform.h
@@ -30,87 +30,86 @@ namespace RNG {
 */
 class UniformDistribution : public RandomDistribution {
 
-    public:
-        /**
-            Creates an uniform distribution with a specific number of bins
-            \param probsCount Number of probability bins in this distribution
-        */
-        UniformDistribution(const uint32_t probsCount) :
-        	RandomDistribution(), probCount(probsCount), deleteDistrib(true) {
+public:
+    /**
+        Creates an uniform distribution with a specific number of bins
+        \param probsCount Number of probability bins in this distribution
+    */
+    UniformDistribution(const uint32_t probsCount) : RandomDistribution(), probCount(probsCount), deleteDistrib(true) {
 
-		if( probCount > 0 ) {
-			probPerBin = 1.0 / static_cast<double>( probCount );
-		}
+        if (probCount > 0) {
+            probPerBin = 1.0 / static_cast<double>(probCount);
+        }
 
-        	baseDistrib = new MersenneRNG();
-    	}
+        baseDistrib = new MersenneRNG();
+    }
 
-    	/**
-	        Creates a Uniform distribution with a specific number of bins and user supplied
-	    	random number generator
-	        \param probsCount Number of probability bins in the distribution
-        	\param baseDist The base random number generator to take the distribution from.
-    	*/
-    	UniformDistribution(const uint32_t probsCount, SST::RNG::Random* baseDist) :
-   		RandomDistribution(), probCount(probsCount), deleteDistrib(false) {
+    /**
+            Creates a Uniform distribution with a specific number of bins and user supplied
+            random number generator
+            \param probsCount Number of probability bins in the distribution
+            \param baseDist The base random number generator to take the distribution from.
+    */
+    UniformDistribution(const uint32_t probsCount, SST::RNG::Random* baseDist)
+        : RandomDistribution(), probCount(probsCount), deleteDistrib(false) {
 
-		if( probCount > 0 ) {
-			probPerBin = 1.0 / static_cast<double>( probCount );
-		}
+        if (probCount > 0) {
+            probPerBin = 1.0 / static_cast<double>(probCount);
+        }
 
-        	baseDistrib = baseDist;
-    	}
+        baseDistrib = baseDist;
+    }
 
-        /**
-            Destroys the distribution and will delete locally allocated RNGs
-        */
-    	~UniformDistribution() {
-        	if(deleteDistrib) {
-	            delete baseDistrib;
-        	}
-    	}
+    /**
+        Destroys the distribution and will delete locally allocated RNGs
+    */
+    ~UniformDistribution() {
+        if (deleteDistrib) {
+            delete baseDistrib;
+        }
+    }
 
-    	/**
-	    Gets the next (random) double value in the distribution
-	    \return The next random double from the distribution, this is the double converted of the index where the probability is located
-    	*/
-    	double getNextDouble() {
-        	const double nextD = baseDistrib->nextUniform();
-		uint32_t current_bin = 1;
+    /**
+        Gets the next (random) double value in the distribution
+        \return The next random double from the distribution, this is the double converted of the index where the
+       probability is located
+    */
+    double getNextDouble() {
+        const double nextD = baseDistrib->nextUniform();
+        uint32_t current_bin = 1;
 
-		while( nextD > ( static_cast<double>( current_bin ) * probPerBin ) ) {
-			current_bin++;
-		}
+        while (nextD > (static_cast<double>(current_bin) * probPerBin)) {
+            current_bin++;
+        }
 
-		return static_cast<double>( current_bin - 1 );
-    	}
+        return static_cast<double>(current_bin - 1);
+    }
 
-    protected:
-        /**
-            Sets the base random number generator for the distribution.
-        */
-        SST::RNG::Random* baseDistrib;
+protected:
+    /**
+        Sets the base random number generator for the distribution.
+    */
+    SST::RNG::Random* baseDistrib;
 
-        /**
-            Controls whether the base distribution should be deleted when this class is destructed.
-        */
-        const bool deleteDistrib;
+    /**
+        Controls whether the base distribution should be deleted when this class is destructed.
+    */
+    const bool deleteDistrib;
 
-        /**
-            Count of discrete probabilities
-        */
-        const uint32_t probCount;
+    /**
+        Count of discrete probabilities
+    */
+    const uint32_t probCount;
 
-	/**
-	    Range 0..1 split into discrete bins
-	*/
-	double probPerBin;
-
+    /**
+        Range 0..1 split into discrete bins
+    */
+    double probPerBin;
 };
 
 using SSTUniformDistribution = SST::RNG::UniformDistribution;
 
-}
-}
+} // namespace RNG
+} // namespace SST
 
 #endif
