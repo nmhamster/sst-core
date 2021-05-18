d77c17018b2e6f5371fc0e34a4b26b7fd24e7952
Si Hammond <sdhammo@sandia.gov>
2021-05-18 09:23:00 -0600

    Fix up formatting

--- src/sst/core/rng/expon.h
+++ src/sst/core/rng/expon.h
@@ -30,77 +30,71 @@ namespace RNG {
 */
 class SSTExponentialDistribution : public SSTRandomDistribution {
 
-    public:
-        /**
-            Creates an exponential distribution with a specific lambda
-            \param mn The lambda of the exponential distribution
-        */
-    SSTExponentialDistribution(const double mn)  :
-    SSTRandomDistribution() {
+public:
+    /**
+        Creates an exponential distribution with a specific lambda
+        \param mn The lambda of the exponential distribution
+    */
+    SSTExponentialDistribution(const double mn) : SSTRandomDistribution() {
 
         lambda = mn;
         baseDistrib = new MersenneRNG();
         deleteDistrib = true;
     }
 
-        /**
-            Creates an exponential distribution with a specific lambda and a base random number generator
-            \param mn The lambda of the exponential distribution
-            \param baseDist The base random number generator to take the distribution from.
-        */
-    SSTExponentialDistribution(const double mn, SST::RNG::Random* baseDist)  :
-    SSTRandomDistribution() {
+    /**
+        Creates an exponential distribution with a specific lambda and a base random number generator
+        \param mn The lambda of the exponential distribution
+        \param baseDist The base random number generator to take the distribution from.
+    */
+    SSTExponentialDistribution(const double mn, SST::RNG::Random* baseDist) : SSTRandomDistribution() {
 
         lambda = mn;
         baseDistrib = baseDist;
         deleteDistrib = false;
     }
 
-        /**
-            Destroys the exponential distribution
-        */
+    /**
+        Destroys the exponential distribution
+    */
     ~SSTExponentialDistribution() {
-        if(deleteDistrib) {
+        if (deleteDistrib) {
             delete baseDistrib;
         }
     }
 
-        /**
-            Gets the next (random) double value in the distribution
-            \return The next random double from the distribution
-        */
-    double getNextDouble()  {
+    /**
+        Gets the next (random) double value in the distribution
+        \return The next random double from the distribution
+    */
+    double getNextDouble() {
         const double next = baseDistrib->nextUniform();
-        return log(1 - next) / ( -1 * lambda );
+        return log(1 - next) / (-1 * lambda);
     }
 
-        /**
-            Gets the lambda with which the distribution was created
-            \return The lambda which the user created the distribution with
-        */
-    double getLambda()  {
-        return lambda;
-    }
+    /**
+        Gets the lambda with which the distribution was created
+        \return The lambda which the user created the distribution with
+    */
+    double getLambda() { return lambda; }
 
+protected:
+    /**
+        Sets the lambda of the exponential distribution.
+    */
+    double lambda;
+    /**
+        Sets the base random number generator for the distribution.
+    */
+    SST::RNG::Random* baseDistrib;
 
-    protected:
-        /**
-            Sets the lambda of the exponential distribution.
-        */
-        double lambda;
-        /**
-            Sets the base random number generator for the distribution.
-        */
-        SST::RNG::Random* baseDistrib;
-
-        /**
-            Controls whether the base distribution should be deleted when this class is destructed.
-        */
-        bool deleteDistrib;
-
+    /**
+        Controls whether the base distribution should be deleted when this class is destructed.
+    */
+    bool deleteDistrib;
 };
 
-}
-}
+} // namespace RNG
+} // namespace SST
 
 #endif
