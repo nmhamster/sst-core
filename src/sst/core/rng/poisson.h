58571578f28645940e19d010810249ef8d442eba
Si Hammond <sdhammo@sandia.gov>
2021-05-18 09:23:11 -0600

    Fix up formatting

--- src/sst/core/rng/poisson.h
+++ src/sst/core/rng/poisson.h
@@ -30,44 +30,42 @@ namespace RNG {
 */
 class PoissonDistribution : public RandomDistribution {
 
-    public:
-        /**
-            Creates an Poisson distribution with a specific lambda
-            \param mn The lambda of the Poisson distribution
-        */
-    PoissonDistribution(const double mn)  :
-    RandomDistribution(), lambda(mn) {
+public:
+    /**
+        Creates an Poisson distribution with a specific lambda
+        \param mn The lambda of the Poisson distribution
+    */
+    PoissonDistribution(const double mn) : RandomDistribution(), lambda(mn) {
 
         baseDistrib = new MersenneRNG();
         deleteDistrib = true;
     }
 
-        /**
-            Creates an Poisson distribution with a specific lambda and a base random number generator
-            \param lambda The lambda of the Poisson distribution
-            \param baseDist The base random number generator to take the distribution from.
-        */
-    PoissonDistribution(const double mn, SST::RNG::Random* baseDist)  :
-    RandomDistribution(), lambda(mn) {
+    /**
+        Creates an Poisson distribution with a specific lambda and a base random number generator
+        \param lambda The lambda of the Poisson distribution
+        \param baseDist The base random number generator to take the distribution from.
+    */
+    PoissonDistribution(const double mn, SST::RNG::Random* baseDist) : RandomDistribution(), lambda(mn) {
 
         baseDistrib = baseDist;
         deleteDistrib = false;
     }
 
-        /**
-            Destroys the Poisson distribution
-        */
-    ~PoissonDistribution()  {
-        if(deleteDistrib) {
+    /**
+        Destroys the Poisson distribution
+    */
+    ~PoissonDistribution() {
+        if (deleteDistrib) {
             delete baseDistrib;
         }
     }
 
-        /**
-            Gets the next (random) double value in the distribution
-            \return The next random double from the distribution
-        */
-     double getNextDouble() {
+    /**
+        Gets the next (random) double value in the distribution
+        \return The next random double from the distribution
+    */
+    double getNextDouble() {
         const double L = exp(-lambda);
         double p = 1.0;
         int k = 0;
@@ -75,39 +73,36 @@ class PoissonDistribution : public RandomDistribution {
         do {
             k++;
             p *= baseDistrib->nextUniform();
-        } while(p > L);
+        } while (p > L);
 
         return k - 1;
     }
 
-        /**
-            Gets the lambda with which the distribution was created
-            \return The lambda which the user created the distribution with
-        */
-    double getLambda()  {
-        return lambda;
-    }
+    /**
+        Gets the lambda with which the distribution was created
+        \return The lambda which the user created the distribution with
+    */
+    double getLambda() { return lambda; }
 
-    protected:
-        /**
-            Sets the lambda of the Poisson distribution.
-        */
-        const double lambda;
-        /**
-            Sets the base random number generator for the distribution.
-        */
-        SST::RNG::Random* baseDistrib;
+protected:
+    /**
+        Sets the lambda of the Poisson distribution.
+    */
+    const double lambda;
+    /**
+        Sets the base random number generator for the distribution.
+    */
+    SST::RNG::Random* baseDistrib;
 
-        /**
-            Controls whether the base distribution should be deleted when this class is destructed.
-        */
-        bool deleteDistrib;
-
+    /**
+        Controls whether the base distribution should be deleted when this class is destructed.
+    */
+    bool deleteDistrib;
 };
 
 using SSTPoissonDistribution = SST::RNG::PoissonDistribution;
 
-}
-}
+} // namespace RNG
+} // namespace SST
 
 #endif
